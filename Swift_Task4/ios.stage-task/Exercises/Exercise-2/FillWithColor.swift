bf47ad9364c957904429a65ec218102f266864a8
Dmitriy Konstantinov <dkonstantinov@i-neti.ru>
2021-06-07 12:23:45 +0300

    feat: added implementation for 1-3 exercise

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -1,8 +1,65 @@
 import Foundation
 
 final class FillWithColor {
-    
-    func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+
+    // MARK: - Types
+    struct RSPoint: Hashable {
+        let row: Int
+        let column: Int
+
+        init(_ row: Int, _ column: Int) {
+            self.row = row
+            self.column = column
+        }
+    }
+
+    // MARK: - Properties
+    private var fillPoints: Set<RSPoint> = []
+
+    // MARK: - Public methods
+    public func fillWithColor(
+        _ image: [[Int]],
+        _ row: Int,
+        _ column: Int,
+        _ newColor: Int
+    ) -> [[Int]] {
+
+        guard !image.isEmpty,
+              image.count <= 50,
+              image.filter({ $0.count != image.first?.count }).isEmpty,
+              image.filter({ !$0.filter({ $0 < 0}).isEmpty }).isEmpty,
+              row >= 0, row < image.count,
+              column >= 0, column < image.first?.count ?? 0,
+              newColor < 65536
+        else { return image }
+
+        var newImage = image
+        fillPoints.insert(.init(row, column))
+        paintAdjacentElements(at: .init(row, column), in: &newImage, with: newColor)
+        fillPoints.forEach { newImage[$0.row][$0.column] = newColor }
+        return newImage
+    }
+
+    // MARK: - Private methods
+    private func paintAdjacentElements(
+        at point: RSPoint,
+        in image: inout [[Int]],
+        with color: Int
+    ) {
+        let area: [RSPoint] = [
+            .init(point.row, point.column - 1),
+            .init(point.row, point.column + 1),
+            .init(point.row - 1, point.column),
+            .init(point.row + 1, point.column)
+        ]
+        .filter { $0.row < image.count && $0.row >= 0 && $0.column < image.first?.count ?? 1 && $0.column >= 0 }
+
+        area.forEach { nearestPoint in
+            if image[nearestPoint.row][nearestPoint.column] == image[point.row][point.column] {
+                if fillPoints.insert(.init(nearestPoint.row, nearestPoint.column)).inserted {
+                    paintAdjacentElements(at: nearestPoint, in: &image, with: color)
+                }
+            }
+        }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,37 +1,144 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
 
+    // MARK: - Properties
+    private var connectedUsers: Set<User> = []
+    private var currentCalls: Set<Call> = []
+}
+
+// MARK: - Station
 extension CallStation: Station {
+
     func users() -> [User] {
-        []
+        Array(connectedUsers)
     }
     
     func add(user: User) {
-
+        connectedUsers.insert(user)
     }
     
     func remove(user: User) {
-
+        if let call = currentCalls.first(where: { $0.incomingUser == user || $0.outgoingUser == user }) {
+            currentCalls.updateCall(call, with: .ended(reason: .error))
+        }
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        switch action {
+        case .start(let incoming, let outgoing):
+            // Проверяем доступность совершения звонка
+            guard connectedUsers.contains(incoming) else { return nil }
+            guard connectedUsers.contains(outgoing)
+            else {
+                return currentCalls.insertCall(incoming: incoming, outgoing: outgoing, status: .ended(reason: .error))
+            }
+            guard currentCalls.filter({ !$0.availabilityOf(users: [incoming, outgoing])}).isEmpty
+            else {
+                return currentCalls.insertCall(incoming: incoming, outgoing: outgoing, status: .ended(reason: .userBusy))
+            }
+            // Запускаем звонок
+            return currentCalls.insertCall(incoming: incoming, outgoing: outgoing, status: .calling)
+
+        case .answer(let outgoingUser):
+            // Проверяем наличие доступных звонков для ответа
+            guard let call = currentCalls.first(where: { $0.outgoingUser == outgoingUser }),
+                  call.status == .calling
+            else { return nil }
+            // Обновляем информацию о звонке
+            return currentCalls.updateCall(call, with: .talk)
+
+        case .end(let user):
+            // Проверяем наличие текущего звонка для пользователя
+            guard let call = currentCalls.first(where: { $0.incomingUser == user || $0.outgoingUser == user}),
+                  let reason = call.status.endReason
+            else { return nil }
+            // Обновляем информацию о звонке
+            return currentCalls.updateCall(call, with: .ended(reason: reason))
+        }
     }
     
     func calls() -> [Call] {
-        []
+        Array(currentCalls)
     }
     
     func calls(user: User) -> [Call] {
-        []
+        currentCalls.filter({ ($0.incomingUser == user || $0.outgoingUser == user) })
     }
     
     func call(id: CallID) -> Call? {
-        nil
+        currentCalls.first(where: { $0.id == id })
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        currentCalls.first(where: { ($0.incomingUser == user || $0.outgoingUser == user) &&
+                            ($0.status == .calling || $0.status == .talk) })
+    }
+}
+
+// MARK: - ext Call
+extension Call {
+
+    func availabilityOf(users: [User]) -> Bool {
+        users.filter({ $0 == incomingUser || $0 == outgoingUser }).isEmpty
+    }
+
+    init(incoming: User, outgoing: User, status: CallStatus) {
+        self.id = CallID()
+        self.incomingUser = incoming
+        self.outgoingUser = outgoing
+        self.status = status
+    }
+
+    init(call: Call, status: CallStatus) {
+        self.id = call.id
+        self.incomingUser = call.incomingUser
+        self.outgoingUser = call.outgoingUser
+        self.status = status
+    }
+}
+
+extension Call: Hashable {
+
+    static func == (lhs: Call, rhs: Call) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id.uuidString)
+    }
+}
+
+// MARK: - ext CallStatus
+extension CallStatus {
+
+    var endReason: CallEndReason? {
+        if self == .calling { return .cancel }
+        if self == .talk { return .end }
+        return nil
+    }
+}
+
+// MARK: - ext User
+extension User: Hashable {
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id.uuidString)
+    }
+}
+
+// MARK: - ext Set<Call>
+extension Set where Element == Call {
+
+    mutating func insertCall(incoming: User, outgoing: User, status: CallStatus) -> CallID {
+        let call = Call(incoming: incoming, outgoing: outgoing, status: status)
+        insert(call)
+        return call.id
+    }
+
+    @discardableResult
+    mutating func updateCall(_ call: Call, with status: CallStatus) -> CallID {
+        update(with: Call(call: call, status: status))
+        return call.id
     }
 }
